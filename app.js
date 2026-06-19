84f194bd5799b49c84c34bfdfad1cce1e72e3676
Matias E. Ramón <matias21siglo@gmail.com>
2026-06-18 22:21:39 -0300

    Trabajo Practico 3

--- app.js
+++ app.js
@@ -38,11 +38,30 @@ if (num1 > num2) {
     alert("Ambos números son iguales.");
 }
 
+// Trabajo Práctico 3
+/*Pide al usuario tres notas con decimales.  
+Convierte las entradas a números decimales.
+Calcula el promedio de las tres notas usando operadores aritméticos.  
+Muestra el promedio en la consola.  
+Usa un condicional para indicar si el promedio es suficiente para aprobar (ejemplo: mayor o igual a 6).
+*/
+let nota1 = parsefloat(prompt("Ingrese la primera nota:"));
+let nota2 = parsefloat(prompt("Ingrese la segunda nota:"));
+let nota3 = parsefloat(prompt("Ingrese la tercera nota:"));
 
+let promedio = (nota1 + nota2 + nota3) / 3;     
+alert("El promedio de las notas es: " + promedio.toFixed(2));
 
+if (promedio == 10) {
+   console.log("🌟¡Felicidades! Destacado.");
+} else if (promedio >= 8) {
+    console.log("🥳 Muy bien.");
+} else if (promedio >= 6) {
+    console.log("🙂 Aprobado.");
+} else if (promedio >= 4) {
+    console.log("😕 Aún puedes mejorar.");
+} else {
+    console.log("😞 Necesitas esforzarte más.");
+}
 
-
-
-
-
-
+// conaulta ...por que el practico 3 no me funciona o no me tira el promedio.
